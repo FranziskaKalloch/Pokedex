8d54eb46fc5890b387786a03ba7fefaecbe3c24b
Franziska Kalloch <franzy@AirvonFranziska.fritz.box>
2026-04-01 10:41:24 +0200

    feat: build function for open and close dialog

---
+++ scripts/comments.js
@@ -0,0 +1,6 @@
+//	1.	init()
+//	2.	Pokémon-Daten laden
+//	3.	Daten verarbeiten
+//	4.	Karten rendern
+//	5.	Button für mehr laden
+//	6.	Overlay öffnen
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
 function init() {
   console.log('Pokedex startet');
 }
+
+function openDialog() {
+  const dialog = document.getElementById('pokemonDialog');
+  dialog.showModal();
+}
+
+function closeDialog() {
+  const dialog = document.getElementById('pokemonDialog');
+  dialog.close();
+}
+
+//	1.	init()
+//	2.	Pokémon-Daten laden
+//	3.	Daten verarbeiten
+//	4.	Karten rendern
+//	5.	Button für mehr laden
+//	6.	Overlay öffnen
